39f9c0cf4ef2f16a5517d6b84b7d66b52394f92d
Ryan Clogg <ryan.clogg@gmail.com>
2018-05-11 15:17:48 +0800

    Complete - still ugly but she works

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
   patch "tasks/:id", to: "tasks#update"
 
-  delete "tasks/:id", to: "tasks#destroy"
+  delete "tasks/:id", to: "tasks#destroy", as: "delete_task"
 end
